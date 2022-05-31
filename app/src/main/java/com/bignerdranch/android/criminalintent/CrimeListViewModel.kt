1ec221f4c879d1beae6fa9d067d994fa40998cd3
ScherbakovMike <62207753+ScherbakovMike@users.noreply.github.com>
2022-05-31 19:48:41 +0300

    refac

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -2,35 +2,23 @@ package com.bignerdranch.android.criminalintent
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
+import com.bignerdranch.android.criminalintent.database.CrimeRepository
 import kotlinx.coroutines.launch
-import java.util.*
-import kotlin.random.Random
 
 class CrimeListViewModel : ViewModel() {
+
+    private val crimeRepository = CrimeRepository.get()
 
     val crimes = mutableListOf<Crime>()
 
     init {
         viewModelScope.launch {
-            crimes+=loadCrimes()
+            crimes += loadCrimes()
         }
     }
 
-    suspend fun loadCrimes():List<Crime> {
-        val result = mutableListOf<Crime>()
-        delay(5000)
-        for (i in 0 until 100) {
-            val crime = Crime(
-                id = UUID.randomUUID(),
-                title = "Crime #$i",
-                isSolved = i % 2 == 0,
-                requiresPolice = Random.nextBoolean()
-            )
-
-            result.add(crime)
-        }
-        return result
+    suspend fun loadCrimes(): List<Crime> {
+        return crimeRepository.getCrimes()
     }
 
 }
